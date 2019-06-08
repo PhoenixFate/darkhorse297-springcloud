f7fefcc967ae0c5ffb898e4f4acd558f1db7bbfe
PhoenixFate <sm516116978@outlook.com>
2019-06-09 01:25:09 +0800

    add consumer module

--- consumer/src/main/java/com/example/ConsumerApplication.java
+++ consumer/src/main/java/com/example/ConsumerApplication.java
@@ -2,14 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-@MapperScan("com.example.mapper")
-public class DemoApplication {
+public class ConsumerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(ConsumerApplication.class, args);
     }
 
 }
---
+++ consumer/src/main/java/com/example/config/RestTemplateConfiguration.java
@@ -0,0 +1,15 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfiguration {
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+//		默认空参数的RestTemplate即为HttpUrlConnection
+        return new RestTemplate();
+    }
+}
