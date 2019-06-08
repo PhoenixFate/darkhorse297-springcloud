be9465a96fb0289d435d3a46ff5eb016ef078d22
PhoenixFate <sm516116978@outlook.com>
2019-06-09 00:50:15 +0800

    user-service

---
+++ user-service/src/main/java/com/example/web/UserController.java
@@ -0,0 +1,23 @@
+package com.example.web;
+
+import com.example.pojo.User;
+import com.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("{id}")
+    public User getUserById(@PathVariable("id") Integer id){
+        return userService.findByQueryById(id);
+    }
+
+}
