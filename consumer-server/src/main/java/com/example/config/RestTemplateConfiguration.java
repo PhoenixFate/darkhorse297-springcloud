8cae2786e73c209b139a263d58a9026f95294c0d
PhoenixFate <sm516116978@outlook.com>
2019-06-09 21:44:21 +0800

    ribbon

--- consumer-server/src/main/java/com/example/config/RestTemplateConfiguration.java
+++ consumer-server/src/main/java/com/example/config/RestTemplateConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfiguration {
 
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate(){
 //		默认空参数的RestTemplate即为HttpUrlConnection
         return new RestTemplate();
--- consumer-server/src/main/java/com/example/web/ConsumerController.java
+++ consumer-server/src/main/java/com/example/web/ConsumerController.java
@@ -4,6 +4,7 @@ import com.example.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonLoadBalancerClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,9 @@ public class ConsumerController {
     @Autowired
     private DiscoveryClient discoveryClient;
 
+//    @Autowired
+//    private RibbonLoadBalancerClient ribbonLoadBalancerClient;
+
     @RequestMapping("{id}")
     public User queryById(@PathVariable Integer id){
         //根据服务id获取服务（list）
@@ -29,6 +33,26 @@ public class ConsumerController {
         ServiceInstance serviceInstance = instances.get(0);
         String url="http://"+serviceInstance.getHost()+":"+serviceInstance.getPort()+"/user/"+id;
         System.out.println(url);
+        User user=restTemplate.getForObject(url,User.class);
+        return user;
+    }
+
+//    @RequestMapping("/ribbon/{id}")
+//    public User queryByIdWithRibbon(@PathVariable Integer id){
+//        //ribbon自动实现负载均衡
+//        ServiceInstance serviceInstance = ribbonLoadBalancerClient.choose("user-service");
+//        String url="http://"+serviceInstance.getHost()+":"+serviceInstance.getPort()+"/user/"+id;
+//        User user=restTemplate.getForObject(url,User.class);
+//        return user;
+//    }
+
+    /*
+    * @LoadBalanced 自动拦截RestTemplate ，实现负载均衡
+    * */
+    @RequestMapping("/loadBalanced/{id}")
+    public User queryByIdWithLoadBalanced(@PathVariable Integer id){
+
+        String url="http://user-service/user/"+id;
         User user=restTemplate.getForObject(url,User.class);
         return user;
     }
