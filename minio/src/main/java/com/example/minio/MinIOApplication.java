3e1034947ec9357f7c18dc292a534ad0d739b619
phoenix fate <sm516116978@outlook.com>
2019-06-13 22:43:09 +0800

    minio

---
+++ minio/src/main/java/com/example/minio/MinIOApplication.java
@@ -0,0 +1,13 @@
+package com.example.minio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class MinIOApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinIOApplication.class, args);
+    }
+
+}
