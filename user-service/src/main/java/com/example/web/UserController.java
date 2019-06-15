aab3ddaf932546374dd85750f952348afa34c009
PhoenixFate <sm516116978@outlook.com>
2019-06-15 21:16:15 +0800

    跨域

--- user-service/src/main/java/com/example/web/UserController.java
+++ user-service/src/main/java/com/example/web/UserController.java
@@ -3,10 +3,7 @@ package com.example.web;
 import com.example.pojo.User;
 import com.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("user")
@@ -25,5 +22,18 @@ public class UserController {
 
         return userService.findByQueryById(id);
     }
+
+    @PostMapping("/post")
+    public User postUserById(@RequestParam(value="id",required = false) Integer id){
+        System.out.println("--------id:  "+id);
+//        try {
+//            Thread.sleep(5000 );
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+        return userService.findByQueryById(id);
+    }
+
 
 }
