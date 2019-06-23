f0aeba54054e2cf5d15d99ada1d5fda9efe45dbb
PhoenixFate <sm516116978@outlook.com>
2019-06-23 22:53:57 +0800

    swagger2

--- user-service/src/main/java/com/example/UserApplication.java
+++ user-service/src/main/java/com/example/UserApplication.java
@@ -3,11 +3,13 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("com.example.mapper")
 @EnableDiscoveryClient
+@EnableSwagger2
 public class UserApplication {
 
     public static void main(String[] args) {
--- user-service/src/main/java/com/example/web/UserController.java
+++ user-service/src/main/java/com/example/web/UserController.java
@@ -2,18 +2,30 @@ package com.example.web;
 
 import com.example.pojo.User;
 import com.example.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("user")
+@Api(value = "eureka-provider", description = "用户查询接口")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @GetMapping("{id}")
-    public User getUserById(@PathVariable("id") Integer id){
+    /**
+     * 根据ID查询用户
+     * @param id
+     * @return
+     */
+    @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Integer", paramType = "path")
+    public User getUserById(@PathVariable("id") @ApiParam(value = "主键ID") Integer id){
 //        try {
 //            Thread.sleep(5000 );
 //        } catch (InterruptedException e) {
